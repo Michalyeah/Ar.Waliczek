bb607f0338b61788bd877cc859c14f731c5cfed4
Jeremiasz <>
2026-09-16 08:42:53 +0200

    Zadanie Domowe

---
+++ 3 tydzien z 34/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace ConsoleApp2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> uczniowie = new List<string>();
+
+            listaUczniow(uczniowie);
+            dodajUczniaDoListy(uczniowie);
+            usunZListyUcznia(uczniowie);
+
+            Console.WriteLine("Aktualna lista uczniów");
+            foreach (string uczen in uczniowie)
+            {
+                Console.WriteLine(uczen);
+            }
+            Console.WriteLine("Liczba uczniów: " + uczniowie.Count);
+        }
+
+        public static void listaUczniow(List<string> uczniowie)
+        {
+            uczniowie.Add("Michał");
+            uczniowie.Add("Felipe");
+            uczniowie.Add("Karollo");
+            uczniowie.Add("Patrykson");
+            uczniowie.Add("Kubus");
+
+            Console.WriteLine("Lista uczniów:  ");
+            foreach (string uczen in uczniowie)
+            {
+                Console.WriteLine(uczen);
+            }
+        }
+
+        public static void dodajUczniaDoListy(List<string> uczniowie)
+        {
+            Console.WriteLine("Podaj imie ucznia aby go dodać do listy: ");
+            string imieDodaniaDoListy = Console.ReadLine() ?? "imie puste";
+
+            uczniowie.Add(imieDodaniaDoListy);
+        }
+
+        public static void usunZListyUcznia(List<string> uczniowie)
+        {
+            Console.WriteLine("Podaj imie do usunięcia ucznia: ");
+            string imieDoUsuniecia = Console.ReadLine() ?? "";
+
+            if (uczniowie.Contains(imieDoUsuniecia))
+            {
+                uczniowie.Remove(imieDoUsuniecia);
+                Console.WriteLine($"Usunięto ucznia: {imieDoUsuniecia}");
+            }
+            else
+            {
+                Console.WriteLine("Nie znaleziono takiego ucznia");
+            }
+            
+        }
+    }
+}
---
+++ 3 tydzien z 34/ConsoleApp2/ConsoleApp2Test/Test1.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp2Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void listaUczniowTest()
+        {
+
+            //arrange
+            List<string> uczniowie = new List<string>();
+            uczniowie.Add("Michał");
+            uczniowie.Add("Felipe");
+            uczniowie.Add("Karollo");
+            uczniowie.Add("Patrykson");
+            uczniowie.Add("Kubus");
+
+            List<string> expected = uczniowie;
+
+            List<string> uczniowie1 = new List<string>();
+            uczniowie.Add("Michał");
+            uczniowie.Add("Felipe");
+            uczniowie.Add("Karollo");
+            uczniowie.Add("Patrykson");
+            uczniowie.Add("Kubus");
+
+            
+
+            //act
+            //ConsoleApp2.Program.listaUczniow(uczniowie);
+
+            //assert
+            CollectionAssert.AreEqual(uczniowie1, uczniowie);
+
+        }
+    }
+}
