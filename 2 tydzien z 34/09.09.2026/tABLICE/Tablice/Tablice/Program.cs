c0d5a11583f25ced81f5fd25c7682dcf72ba3a4c
Jeremiasz <>
2026-09-16 07:53:31 +0200

    3 tydzien

---
+++ 2 tydzien z 34/09.09.2026/tABLICE/Tablice/Tablice/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Tablice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            sumowanieDanych();
+            maksimumTablicy();
+            minimumTablicy();
+
+        }
+        public static void sumowanieDanych()
+        {
+            int[] tablica = { 4, 7, 2, 9, 1 };
+            int suma = 0;
+
+            foreach (var item in tablica)
+            {
+                suma += item;
+            }
+            Console.WriteLine(suma);
+        }
+        public static void maksimumTablicy()
+        {
+            int[] tablica = { 4, 7, 2, 9, 1 };
+            int max = tablica[0];
+
+            for (int i = 1; i < tablica.Length; i++) {
+                if (tablica[i] > max)
+                {
+                    max = tablica[i];
+                }
+                Console.WriteLine(max);
+            }
+
+        }
+        public static void minimumTablicy()
+        {
+            int[] tablica = { 4, 7, 2, 9, 1 };
+            int min = tablica[0];
+
+            for (int i = 1; i < tablica.Length; i++)
+            {
+                if (tablica[i] < min)
+                {
+                    min = tablica[i];
+                }
+                Console.WriteLine(min);
+            }
+
+        }
+        public static void macierze()
+        {
+            //Macierze czyli tablice wielowymiarowe
+            /*
+             |1,2,3|
+             |3,4,5|
+             |6,7,8|
+             
+             
+             */
+            int[,] talbica = { { 1, 2, 3 }, { 6, 5, 4 } };
+            for(int wiersze = 0; wiersze < talbica.Length; wiersze++)
+            {
+                for(int kolumny = 0; kolumny < talbica.Length; kolumny++)
+                {
+                    Console.Write(talbica[wiersze,]);
+                }
+            }
+        }
+    }
+}
---
+++ 3 tydzien z 34/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Zadanie1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> uczniowie = new List<string>();
+            uczniowie.Add("Michał");
+            uczniowie.Add("Felipe");
+            uczniowie.Add("Karollo");
+            uczniowie.Add("Patrykson");
+            uczniowie.Add("Kubus");
+
+            Console.WriteLine("Lista uczniów:  ");
+            foreach (string uczen in uczniowie)
+            {
+                Console.WriteLine(uczen);
+            }
+            Console.WriteLine("Podaj imie ucznia aby go dodać do listy: ");
+            string imieDodaniaDoListy = Console.ReadLine() ?? "imie puste";
+            uczniowie.Add(imieDodaniaDoListy);
+            Console.WriteLine("Podaj imie do usunięcia ucznia: ");
+            string imieDoUsuniecia = Console.ReadLine() ?? "";
+
+            if (uczniowie.Contains(imieDoUsuniecia))
+            {
+                uczniowie.Remove(imieDoUsuniecia);
+                Console.WriteLine($"Usunięto ucznia: {imieDoUsuniecia}");
+            }
+            else
+            {
+                Console.WriteLine("Nie znaleziono takiego ucznia");
+            }
+            Console.WriteLine("Aktualna lista uczniów");
+            foreach (string uczen in uczniowie)
+            {
+                Console.WriteLine(uczen);
+            }
+            Console.WriteLine("Liczba uczniów: " + uczniowie.Count);
+        }
+    }
+}
